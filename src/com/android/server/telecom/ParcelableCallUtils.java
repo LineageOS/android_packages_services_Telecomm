1a1655c04253c34c8ac27ff2587db696f17c2963
Danny Baumann <dannybaumann@web.de>
2014-11-13 14:31:37 +0100

    Proper supplementary service notification handling (3/5)
    Pass parsed call properties around instead bleeding the implementation
    detail 'Supplementary service notification' into all layers.
    
    Change-Id: Ib7c3a494024013c261b1d708c47d0e80ae5876dd

--- src/com/android/server/telecom/ParcelableCallUtils.java
+++ src/com/android/server/telecom/ParcelableCallUtils.java
@@ -337,6 +337,21 @@ public class ParcelableCallUtils {
 
         Connection.PROPERTY_SELF_MANAGED,
         android.telecom.Call.Details.PROPERTY_SELF_MANAGED
+
+        Connection.PROPERTY_WAS_FORWARDED,
+        android.telecom.Call.Details.PROPERTY_WAS_FORWARDED,
+
+        Connection.PROPERTY_HELD_REMOTELY,
+        android.telecom.Call.Details.PROPERTY_HELD_REMOTELY,
+
+        Connection.PROPERTY_DIALING_IS_WAITING,
+        android.telecom.Call.Details.PROPERTY_DIALING_IS_WAITING,
+
+        Connection.PROPERTY_ADDITIONAL_CALL_FORWARDED,
+        android.telecom.Call.Details.PROPERTY_ADDITIONAL_CALL_FORWARDED,
+
+        Connection.PROPERTY_REMOTE_INCOMING_CALLS_BARRED,
+        android.telecom.Call.Details.PROPERTY_REMOTE_INCOMING_CALLS_BARRED
     };
 
     private static int convertConnectionToCallProperties(int connectionProperties) {
