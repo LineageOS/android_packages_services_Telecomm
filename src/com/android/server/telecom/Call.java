5507671b2ecba6c2e2de7df1a2177776f4a8db46
Hall Liu <hallliu@google.com>
2017-02-28 19:59:30 -0800

    Redo DtmfLocalTonePlayer
    The system of locks in DtmfLocalTonePlayer and the constant thread
    creation/destruction seems to be behind a system ANR bug. This change
    refactors the class to rely on a single Handler and associated
    HandlerThread that lives for the entire duration of Telecom.
    
    Bug: 34886553
    Test: manual and unit
    Change-Id: I1d86c644aaecdb6ff93d31e2b9e97792e3e301e0

--- src/com/android/server/telecom/Call.java
+++ src/com/android/server/telecom/Call.java
@@ -1183,7 +1183,7 @@ public class Call implements CreateConnectionResponse, EventManager.Loggable {
      *
      * @return The {@link Context}.
      */
-    Context getContext() {
+    public Context getContext() {
         return mContext;
     }
 
