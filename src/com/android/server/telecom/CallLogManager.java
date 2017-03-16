d2c2730b98c5b64fff3559dd3b2ede36fb09e0b9
Olaia Segovia <olaia.segovia@bq.com>
2017-03-16 08:40:30 +0100

    Telecomm: Make sensitive phone numbers not to be shown in call log history.
    Author: Olaia Segovia <olaia.segovia@bq.com>
    Date:   Thu Mar 16 08:40:30 2017 +0100
    
        (1/2) Make sensitive phone numbers not to be shown in call log history.
    
        Considering sensitive phone numbers to be, hotlines dealing with
        violence against women.
        In the EU, these numbers do not show up in the phone bill.
        In order to avoid these phone numbers to be listed in the Call Log, we
        have implemented a new XML file which is parsed with phone numbers
        from different countries to be filtered. This file needs to be copied to
        device via desired Android.mk file in order to be able to process it.
        The comparison is made checking the network MCC the SIM is connected to
        in order to consider roaming and multisim scenarios.
    
        Test: CallLogManagerTest.testDontLogCallsToSensitivePhoneNumber PASS
    
        Change-Id: I4a59ff0577942ce56924f1a434ae0a3a38eacc62
        Signed-off-by: Olaia Segovia <olaia.segovia@bq.com>
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Thu Jun 22 19:40:56 2017 +0200
    
        SensitivePhoneNumbers: Handle lists of MCC codes
    
        * Some countries have multiple MCC codes, so handle it
        * In the sensitivePN network field, enter the list of
          MCCs like so: https://review.lineageos.org/178233
    
        Change-Id: I34225473404b2be2640ea9ab05691dc985c49fa0
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Thu Jul 6 16:12:12 2017 -0500
    
        SensitivePhoneNumbers: Fix number comparison
    
        * Currently, we just compare the strings for equality,
          which results in incorrect detection of sensitive nums
          a lot of the time, because adding (or removing) the
          country code is enough to make the detection fail,
          meaning the call to that phone number is logged
        * Use Android's PhoneNumberUtils comparison method to
          fix this, since it takes these factors into account
    
        Change-Id: I26ac180f8a6552cf87a4bada1d370f0ebb884ee1
    
    Change-Id: Id07a7df56ba23d1bfce5a9b646cfb08ff2ce7bab

--- src/com/android/server/telecom/CallLogManager.java
+++ src/com/android/server/telecom/CallLogManager.java
@@ -145,6 +145,7 @@ public final class CallLogManager extends CallsManagerListenerBase {
 
     private Object mLock;
     private String mCurrentCountryIso;
+    private SensitivePhoneNumbers mSensitivePhoneNumbers;
 
     public CallLogManager(Context context, PhoneAccountRegistrar phoneAccountRegistrar,
             MissedCallNotifier missedCallNotifier) {
@@ -154,6 +155,7 @@ public final class CallLogManager extends CallsManagerListenerBase {
         mPhoneAccountRegistrar = phoneAccountRegistrar;
         mMissedCallNotifier = missedCallNotifier;
         mLock = new Object();
+        mSensitivePhoneNumbers = new SensitivePhoneNumbers();
     }
 
     @Override
@@ -396,9 +398,15 @@ public final class CallLogManager extends CallsManagerListenerBase {
                     CarrierConfigManager.KEY_ALLOW_EMERGENCY_NUMBERS_IN_CALL_LOG_BOOL);
         }
 
+
+        // Don't log sensitive numbers.
+        boolean isSensitiveNumber = mSensitivePhoneNumbers.isSensitiveNumber(mContext, number,
+                accountHandle.getId());
+        Log.d(TAG, "isSensitiveNumber: " + isSensitiveNumber);
+
         // Don't log emergency numbers if the device doesn't allow it.
         final boolean isOkToLogThisCall = (!isEmergency || okToLogEmergencyNumber)
-                && !isUnloggableNumber(number, configBundle);
+                && !isUnloggableNumber(number, configBundle) && !isSensitiveNumber;
 
         sendAddCallBroadcast(callType, duration);
 
