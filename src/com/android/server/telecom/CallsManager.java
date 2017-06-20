59a314c44c0d5ad56c99943cfa26369b46665475
Tony Mak <tonymak@google.com>
2017-06-20 16:31:44 +0100

    Fix NPE when addingIncomingCall is called with null extras
    Fix: b/62815961
    
    Test: cts-tradefed run cts-dev --module DevicePolicyManager --test  com.android.cts.devicepolicy.ManagedProfileTest#testManagedCall
    
    Change-Id: I3b460d8d5e005201ee08d78fa3d40a909c487c20

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -866,8 +866,7 @@ public class CallsManager extends Call.ListenerBase
                 }
             }
 
-            if (phoneAccount.getExtras()
-                    .getBoolean(PhoneAccount.EXTRA_ALWAYS_USE_VOIP_AUDIO_MODE)) {
+            if (extras.getBoolean(PhoneAccount.EXTRA_ALWAYS_USE_VOIP_AUDIO_MODE)) {
                 Log.d(this, "processIncomingCallIntent: defaulting to voip mode for call %s",
                         call.getId());
                 call.setIsVoipAudioMode(true);
