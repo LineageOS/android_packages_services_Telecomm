3490e5f9b1671f533adebd72a6bcaf3de7140a66
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:35:10 +0800

    Telecomm: Incall vibration options [2/3]
    * allow setting vibration when call is connected
    * allow setting vibration when call is disconnected
    * allow setting vibration for call waiting
    * this works with google and aosp dialer :)
    
    Change-Id: I1dcdf20d022df94cf798548f0c3e2d524f8b21ca

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -48,9 +48,12 @@ import android.os.RemoteException;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.os.Vibrator;
+import android.provider.Settings;
 import android.telecom.CallAudioState;
 import android.telecom.CallEndpoint;
 import android.telecom.ConnectionService;
+import android.telecom.DisconnectCause;
 import android.telecom.InCallService;
 import android.telecom.Log;
 import android.telecom.Logging.Runnable;
@@ -95,6 +98,12 @@ public class InCallController extends CallsManagerListenerBase implements
     public static final String NOTIFICATION_TAG = InCallController.class.getSimpleName();
     public static final int IN_CALL_SERVICE_NOTIFICATION_ID = 3;
     private AnomalyReporterAdapter mAnomalyReporter = new AnomalyReporterAdapterImpl();
+
+    private static final long[] INCALL_VIBRATION_PATTERN = {
+            100,
+            200,
+            0,
+    };
 
     /**
      * Anomaly Report UUIDs and corresponding error descriptions specific to InCallController.
@@ -1665,6 +1674,17 @@ public class InCallController extends CallsManagerListenerBase implements
         Log.i(this, "onCallStateChanged: Call state changed for TC@%s: %s -> %s", call.getId(),
                 CallState.toString(oldState), CallState.toString(newState));
         maybeTrackMicrophoneUse(isMuted());
+        boolean vibrateOnConnect = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.VIBRATE_ON_CONNECT, 0, UserHandle.USER_CURRENT) >= 1;
+        boolean vibrateOnDisconnect = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.VIBRATE_ON_DISCONNECT, 0, UserHandle.USER_CURRENT) >= 1;
+
+        if (oldState == CallState.DIALING && newState == CallState.ACTIVE && vibrateOnConnect) {
+            vibrate(INCALL_VIBRATION_PATTERN);
+        } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED
+                && vibrateOnDisconnect) {
+            vibrate(INCALL_VIBRATION_PATTERN);
+        }
         updateCall(call);
     }
 
@@ -3325,5 +3345,9 @@ public class InCallController extends CallsManagerListenerBase implements
             }
         }
         return false;
+    }
+
+    public void vibrate(long[] pattern) {
+        ((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
     }
 }
--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -42,7 +42,9 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.os.vibrator.persistence.ParsedVibration;
 import android.os.vibrator.persistence.VibrationXmlParser;
+import android.provider.Settings;
 import android.telecom.Log;
+import android.telecom.DisconnectCause;
 import android.telecom.TelecomManager;
 import android.util.Pair;
 import android.view.accessibility.AccessibilityManager;
@@ -153,6 +155,12 @@ public class Ringer {
             0, // No delay before starting
             255, // Vibrate full amplitude
             0, // No amplitude while waiting
+    };
+
+    private static final long[] CALL_WAITING_VIBRATION_PATTERN = {
+            200,
+            300,
+            500,
     };
 
     /**
@@ -596,6 +604,13 @@ public class Ringer {
         Log.v(this, "Playing call-waiting tone.");
 
         stopRinging();
+
+       if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT) >= 1) {
+            if (mVibrator.hasVibrator()) {
+                mVibrator.vibrate(CALL_WAITING_VIBRATION_PATTERN, -1);
+            }
+        }
 
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
