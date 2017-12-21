fa5faef4a25315253e674391da587f78e162b890
Hall Liu <hallliu@google.com>
2017-12-21 15:43:27 -0800

    Reset speakerphone at beginning and end of calls
    Always call AudioManager#setSpeakerphoneOn(false) at the beginning and
    end of calls.
    
    Bug: 69305994
    Bug: 70338906
    Test: unit
    Change-Id: Ifcd887ca7bdeab8cff8a2773a306548142979238
    Merged-In: Ifcd887ca7bdeab8cff8a2773a306548142979238
    (cherry picked from commit 0814db7bf10de1c51c7f4bd15b70c6f6eebd5dac)

--- src/com/android/server/telecom/CallAudioRouteStateMachine.java
+++ src/com/android/server/telecom/CallAudioRouteStateMachine.java
@@ -1293,11 +1293,9 @@ public class CallAudioRouteStateMachine extends StateMachine {
     }
 
     private void setSpeakerphoneOn(boolean on) {
-        if (mAudioManager.isSpeakerphoneOn() != on) {
-            Log.i(this, "turning speaker phone %s", on);
-            mAudioManager.setSpeakerphoneOn(on);
-            mStatusBarNotifier.notifySpeakerphone(on);
-        }
+        Log.i(this, "turning speaker phone %s", on);
+        mAudioManager.setSpeakerphoneOn(on);
+        mStatusBarNotifier.notifySpeakerphone(on);
     }
 
     private void setBluetoothOn(boolean on) {
@@ -1504,6 +1502,7 @@ public class CallAudioRouteStateMachine extends StateMachine {
         mDeviceSupportedRoutes = initState.getSupportedRouteMask();
         mAvailableRoutes = mDeviceSupportedRoutes & getCurrentCallSupportedRoutes();
         mIsMuted = initState.isMuted();
+        setSpeakerphoneOn(initState.getRoute() == CallAudioState.ROUTE_SPEAKER);
         setMuteOn(mIsMuted);
         mWasOnSpeaker = false;
         mHasUserExplicitlyLeftBluetooth = false;
