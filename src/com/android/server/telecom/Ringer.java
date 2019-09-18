56871f54c31da9dc6a75a1dac98a15ee76e796fc
Danny Baumann <dannybaumann@web.de>
2019-09-18 08:58:56 +0200

    Add back increasing ring feature (1/2).
    Change-Id: I1d5173fb8134400e5fa72c19181f34e1a5ec4e0d

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -19,6 +19,7 @@ package com.android.server.telecom;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Person;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
@@ -35,6 +36,8 @@ import android.telecom.TelecomManager;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.telecom.LogUtils.EventTimer;
+
+import lineageos.providers.LineageSettings;
 
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
@@ -77,6 +80,9 @@ public class Ringer {
 
     private static final int RAMPING_RINGER_VIBRATION_DURATION = 5000;
     private static final int RAMPING_RINGER_DURATION = 10000;
+
+    private int mRampingRingerDuration = -1;  // ramping ringer duration in millisecond
+    private float mRampingRingerStartVolume = 0f;
 
     static {
         // construct complete pulse pattern
@@ -293,9 +299,30 @@ public class Ringer {
                 hapticsFuture = mRingtonePlayer.play(mRingtoneFactory, foregroundCall,
                         mVolumeShaperConfig, attributes.isRingerAudible(), isVibratorEnabled);
             } else {
-                // Ramping ringtone is not enabled.
-                hapticsFuture = mRingtonePlayer.play(mRingtoneFactory, foregroundCall, null,
-                        attributes.isRingerAudible(), isVibratorEnabled);
+                final ContentResolver cr = mContext.getContentResolver();
+                if (LineageSettings.System.getInt(cr,
+                        LineageSettings.System.INCREASING_RING, 0) != 0) {
+                    float startVolume = LineageSettings.System.getFloat(cr,
+                            LineageSettings.System.INCREASING_RING_START_VOLUME, 0.1f);
+                    int rampUpTime = LineageSettings.System.getInt(cr,
+                            LineageSettings.System.INCREASING_RING_RAMP_UP_TIME, 20);
+                    if (mVolumeShaperConfig == null
+                        || mRampingRingerDuration != rampUpTime
+                        || mRampingRingerStartVolume != startVolume) {
+                        mVolumeShaperConfig = new VolumeShaper.Configuration.Builder()
+                            .setDuration(rampUpTime * 1000)
+                            .setCurve(new float[] {0.f, 1.f}, new float[] {startVolume, 1.f})
+                            .setInterpolatorType(VolumeShaper
+                                    .Configuration.INTERPOLATOR_TYPE_LINEAR)
+                            .build();
+                        mRampingRingerDuration = rampUpTime;
+                        mRampingRingerStartVolume = startVolume;
+                    }
+                } else {
+                    mVolumeShaperConfig = null;
+                }
+                hapticsFuture = mRingtonePlayer.play(mRingtoneFactory, foregroundCall,
+                        mVolumeShaperConfig, attributes.isRingerAudible(), isVibratorEnabled);
                 effect = getVibrationEffectForCall(mRingtoneFactory, foregroundCall);
             }
         } else {
