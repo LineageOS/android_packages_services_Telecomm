8a08eba526ce7e7828494bab2e32f53ebb3599df
Danny Baumann <dannybaumann@web.de>
2019-09-18 08:58:56 +0200

    Add back increasing ring feature (1/2).
    Change-Id: I1d5173fb8134400e5fa72c19181f34e1a5ec4e0d

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -26,6 +26,7 @@ import android.annotation.Nullable;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Person;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.media.AudioAttributes;
@@ -53,6 +54,8 @@ import android.view.accessibility.AccessibilityManager;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.telecom.LogUtils.EventTimer;
 import com.android.server.telecom.flags.FeatureFlags;
+
+import lineageos.providers.LineageSettings;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -402,6 +405,23 @@ public class Ringer {
                 } else {
                     if (DEBUG_RINGER) {
                         Log.i(this, "Create ringer with custom vibration effect");
+                    }
+                    final ContentResolver cr = mContext.getContentResolver();
+                    if (LineageSettings.System.getInt(cr,
+                            LineageSettings.System.INCREASING_RING, 0) != 0) {
+                        float startVolume = LineageSettings.System.getFloat(cr,
+                                LineageSettings.System.INCREASING_RING_START_VOLUME, 0.1f);
+                        int rampUpTime = LineageSettings.System.getInt(cr,
+                                LineageSettings.System.INCREASING_RING_RAMP_UP_TIME, 20);
+                        mVolumeShaperConfig =
+                                new VolumeShaper.Configuration.Builder()
+                                        .setDuration(rampUpTime * 1000)
+                                        .setCurve(
+                                                new float[]{0.f, 1.f},
+                                                new float[]{startVolume, 1.f})
+                                        .setInterpolatorType(
+                                                VolumeShaper.Configuration.INTERPOLATOR_TYPE_LINEAR)
+                                        .build();
                     }
                     // Ramping ringtone is not enabled.
                     useCustomVibrationEffect = true;
