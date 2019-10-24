45e18615ad173240375f00acdc37ba8832ababef
Yiwen Chen <evanchen@google.com>
2019-10-24 15:14:55 -0700

    Consistent vibration for ramping ringer on P4
    On P4 devices, we currently can only do audio-coupled haptics with
    ramping ringer. This cl fixes the bug. It gives us option to play normal
    vibration with ramping ringer on P4 so that we can have consistent
    function across all Pixel devices.
    
    Bug:143305848
    Test: tested on all Pixel devices.
    Change-Id: I3e50fbeb8878b9620b7807ba53a650e38a3280ad

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -332,6 +332,12 @@ public class Ringer {
                             isUsingAudioCoupledHaptics, mIsHapticPlaybackSupportedByDevice);
                     maybeStartVibration(foregroundCall, shouldRingForContact, effect,
                             isVibratorEnabled, isRingerAudible);
+                } else if (mSystemSettingsUtil.applyRampingRinger(mContext)
+                           && mSystemSettingsUtil.enableRampingRingerFromDeviceConfig()
+                           && !mSystemSettingsUtil.enableAudioCoupledVibrationForRampingRinger()) {
+                    Log.i(this, "startRinging: apply ramping ringer vibration");
+                    maybeStartVibration(foregroundCall, shouldRingForContact, effect,
+                            isVibratorEnabled, isRingerAudible);
                 } else {
                     Log.addEvent(foregroundCall, LogUtils.Events.SKIP_VIBRATION,
                             "using audio-coupled haptics");
@@ -354,7 +360,6 @@ public class Ringer {
 
     private void maybeStartVibration(Call foregroundCall, boolean shouldRingForContact,
         VibrationEffect effect, boolean isVibrationEnabled, boolean isRingerAudible) {
-
         if (isVibrationEnabled
                 && !mIsVibrating && shouldRingForContact) {
             if (mSystemSettingsUtil.applyRampingRinger(mContext)
