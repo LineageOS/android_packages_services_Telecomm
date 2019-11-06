ea80d645299954cd2f7d382c93a2c52c7def8798
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-06 10:58:16 -0600

    [SQUASH] Merge android-10.0.0_r11 into lineage-17.0
    Change-Id: I2a4317ad5263736dc4b466604ba80ec5ed5469a5

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -400,6 +400,7 @@ public class CallsManager extends Call.ListenerBase
             Context context = args[0].first;
             BlockedNumbersUtil.updateEmergencyCallNotification(context,
                     SystemContract.shouldShowEmergencyCallNotification(context));
+            Log.endSession();
             return null;
         }
     }
