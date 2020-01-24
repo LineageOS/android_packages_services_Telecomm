6306f47df7eef4af262352f126154062ca709e6c
Grace Jia <xiaotonj@google.com>
2020-01-24 12:10:20 -0800

    Capture metrics on InCallService failures and inform user of failures
    Test: Manual test on telecom testapps, atest TelecomUnitTest
    Bug: 147883088
    Change-Id: Id0e11558c16d770156fd01e470969c99e956be45

--- src/com/android/server/telecom/TelecomSystem.java
+++ src/com/android/server/telecom/TelecomSystem.java
@@ -270,7 +270,7 @@ public class TelecomSystem {
                     EmergencyCallHelper emergencyCallHelper) {
                 return new InCallController(context, lock, callsManager, systemStateProvider,
                         defaultDialerCache, timeoutsAdapter, emergencyCallHelper,
-                        new CarModeTracker());
+                        new CarModeTracker(), clockProxy);
             }
         };
 
