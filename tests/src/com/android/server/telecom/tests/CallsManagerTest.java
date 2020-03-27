708de81764872887a0db3e2e4a40f23780910e40
Tyler Gunn <tgunn@google.com>
2020-03-26 18:25:20 -0700

    Correct call swapping logic to account for connection manager.
    Some connection managers have been known to add connections using their
    own phone accounts other than the telephony ones. This causes a problem
    when trying to unhold one call and hold another because one call could be
    from telephony, and another from the connection manager.
    
    Further, there was an issue where existing connections and conferences
    were not being assigned a connection manager.
    
    This bug:
    1. Redefines what it means to be from the same "source" as another call.
    In the context of telecom we assume that the target phone accounts
    are the same package (existing behavior) they're the same source.  We
    also assume if the calls have the same package for their connection
    manager, they're from the same source.  This allows for scenarios like
    the one described above.
    2. Ensures existing connections and conferences are assigned a connection
    manager phone account.  This is inferred using existing logic in the
    PHoneAccountRegistrar which can look up a sim call manager based on the
    target phone account for a call.  The logic is enhanced so that if a
    target phone account is in fact already a connection manager, it is found
    as well.
    3. Updates the telecom event logging to log the connection manager, and
    also updates telecom Call toString to better log details on the calls.
    
    Test: Performed manual live network testing with connection manager to
    ensure correct assignment of connection mgr phone accounts in dumpsys.
    Test: Wrote unit test to verify same source matching.
    Test: Wrote unit test to verify that the hold/unhold swap logic works when
    the connection manager matches but the target phone account does not.
    Test: 151221531
    Fixes: 153376238
    
    Change-Id: I263e706e8b6a99a9a79fb63dfc7dc10f4b271eb5
    Merged-In: Id8366257edb68a6a3b153a8645b1eb062b7f6b0b

--- tests/src/com/android/server/telecom/tests/CallsManagerTest.java
+++ tests/src/com/android/server/telecom/tests/CallsManagerTest.java
@@ -124,6 +124,10 @@ public class CallsManagerTest extends TelecomTestCase {
             ComponentName.unflattenFromString("com.foo/.Blah"), "Sim1");
     private static final PhoneAccountHandle SIM_2_HANDLE = new PhoneAccountHandle(
             ComponentName.unflattenFromString("com.foo/.Blah"), "Sim2");
+    private static final PhoneAccountHandle CONNECTION_MGR_1_HANDLE = new PhoneAccountHandle(
+            ComponentName.unflattenFromString("com.bar/.Conn"), "Cm1");
+    private static final PhoneAccountHandle CONNECTION_MGR_2_HANDLE = new PhoneAccountHandle(
+            ComponentName.unflattenFromString("com.spa/.Conn"), "Cm2");
     private static final PhoneAccountHandle VOIP_1_HANDLE = new PhoneAccountHandle(
             ComponentName.unflattenFromString("com.voip/.Stuff"), "Voip1");
     private static final PhoneAccountHandle SELF_MANAGED_HANDLE = new PhoneAccountHandle(
@@ -1288,6 +1292,68 @@ public class CallsManagerTest extends TelecomTestCase {
         verify(screenedCall).setAudioProcessingRequestingApp(appName);
     }
 
+    /**
+     * Verify the behavior of the {@link CallsManager#areFromSameSource(Call, Call)} method.
+     * @throws Exception
+     */
+    @SmallTest
+    @Test
+    public void testAreFromSameSource() throws Exception {
+        Call callSim1 = createCall(SIM_1_HANDLE, null, CallState.ACTIVE);
+        Call callSim2 = createCall(SIM_2_HANDLE, null, CallState.ACTIVE);
+        Call callVoip1 = createCall(VOIP_1_HANDLE, null, CallState.ACTIVE);
+        assertTrue(CallsManager.areFromSameSource(callSim1, callSim1));
+        assertTrue(CallsManager.areFromSameSource(callSim1, callSim2));
+        assertFalse(CallsManager.areFromSameSource(callSim1, callVoip1));
+        assertFalse(CallsManager.areFromSameSource(callSim2, callVoip1));
+
+        Call callSim1ConnectionMgr1 = createCall(SIM_1_HANDLE, CONNECTION_MGR_1_HANDLE,
+                CallState.ACTIVE);
+        Call callSim2ConnectionMgr2 = createCall(SIM_2_HANDLE, CONNECTION_MGR_2_HANDLE,
+                CallState.ACTIVE);
+        assertFalse(CallsManager.areFromSameSource(callSim1ConnectionMgr1, callVoip1));
+        assertFalse(CallsManager.areFromSameSource(callSim2ConnectionMgr2, callVoip1));
+        // Even though the connection manager differs, the underlying telephony CS is the same
+        // so hold/swap will still work as expected.
+        assertTrue(CallsManager.areFromSameSource(callSim1ConnectionMgr1, callSim2ConnectionMgr2));
+
+        // Sometimes connection managers have been known to also have calls
+        Call callConnectionMgr = createCall(CONNECTION_MGR_2_HANDLE, CONNECTION_MGR_2_HANDLE,
+                CallState.ACTIVE);
+        assertTrue(CallsManager.areFromSameSource(callSim2ConnectionMgr2, callConnectionMgr));
+    }
+
+    /**
+     * Ensures that if we have two calls hosted by the same connection manager, but with
+     * different target phone accounts, we can swap between them.
+     * @throws Exception
+     */
+    @SmallTest
+    @Test
+    public void testSwapCallsWithSameConnectionMgr() throws Exception {
+        // GIVEN a CallsManager with ongoing call, and this call can not be held
+        Call ongoingCall = addSpyCall(SIM_1_HANDLE, CONNECTION_MGR_1_HANDLE, CallState.ACTIVE);
+        doReturn(false).when(ongoingCall).can(Connection.CAPABILITY_HOLD);
+        doReturn(true).when(ongoingCall).can(Connection.CAPABILITY_SUPPORT_HOLD);
+        when(mConnectionSvrFocusMgr.getCurrentFocusCall()).thenReturn(ongoingCall);
+
+        // and a held call which has the same connection manager, but a different target phone
+        // account.  We have seen cases where a connection mgr adds its own calls and these can
+        // be problematic for swapping.
+        Call heldCall = addSpyCall(CONNECTION_MGR_1_HANDLE, CONNECTION_MGR_1_HANDLE,
+                CallState.ON_HOLD);
+
+        // WHEN unhold the held call
+        mCallsManager.unholdCall(heldCall);
+
+        // THEN the ongoing call is held
+        verify(ongoingCall).hold(any());
+        verifyFocusRequestAndExecuteCallback(heldCall);
+
+        // and held call is unhold now
+        verify(heldCall).unhold(any());
+    }
+
     private Call addSpyCall() {
         return addSpyCall(SIM_2_HANDLE, CallState.ACTIVE);
     }
@@ -1297,7 +1363,12 @@ public class CallsManagerTest extends TelecomTestCase {
     }
 
     private Call addSpyCall(PhoneAccountHandle targetPhoneAccount, int initialState) {
-        Call ongoingCall = createCall(targetPhoneAccount, initialState);
+        return addSpyCall(targetPhoneAccount, null, initialState);
+    }
+
+    private Call addSpyCall(PhoneAccountHandle targetPhoneAccount,
+            PhoneAccountHandle connectionMgrAcct, int initialState) {
+        Call ongoingCall = createCall(targetPhoneAccount, connectionMgrAcct, initialState);
         Call callSpy = Mockito.spy(ongoingCall);
 
         // Mocks some methods to not call the real method.
@@ -1312,6 +1383,11 @@ public class CallsManagerTest extends TelecomTestCase {
     }
 
     private Call createCall(PhoneAccountHandle targetPhoneAccount, int initialState) {
+        return createCall(targetPhoneAccount, null /* connectionManager */, initialState);
+    }
+
+    private Call createCall(PhoneAccountHandle targetPhoneAccount,
+            PhoneAccountHandle connectionManagerAccount, int initialState) {
         Call ongoingCall = new Call(String.format("TC@%d", sCallId++), /* callId */
                 mContext,
                 mCallsManager,
@@ -1320,7 +1396,7 @@ public class CallsManagerTest extends TelecomTestCase {
                 mPhoneNumberUtilsAdapter,
                 TEST_ADDRESS,
                 null /* GatewayInfo */,
-                null /* connectionManagerPhoneAccountHandle */,
+                connectionManagerAccount,
                 targetPhoneAccount,
                 Call.CALL_DIRECTION_INCOMING,
                 false /* shouldAttachToExistingConnection*/,
