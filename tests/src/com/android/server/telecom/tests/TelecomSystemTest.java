df68d921e880ac70b55bbd322cdb5679a252200c
chapin <chapin@google.com>
2021-01-07 17:00:29 +0000

    Revert "Bluetooth updateability: Move BluetoothPhoneService out ..."
    Revert "Updateability: Remove IBluetoothHeadsetPhone aidl"
    
    Revert submission 1311861-BluetoothInCallService
    
    Reason for revert: Bug: 176883407
    Reverted Changes:
    Ie3a5ceda5:UpdateAbility: Implement BluetoothInCallService an...
    I2e3bc64eb:Updateability: Remove IBluetoothHeadsetPhone aidl
    If26bab4ad:Bluetooth updateability: Move BluetoothPhoneServic...
    
    Change-Id: Id9649e78d447a6c360860bcdde3069bb9b09c89f

--- tests/src/com/android/server/telecom/tests/TelecomSystemTest.java
+++ tests/src/com/android/server/telecom/tests/TelecomSystemTest.java
@@ -69,6 +69,7 @@ import android.text.TextUtils;
 
 import com.android.internal.telecom.IInCallAdapter;
 import com.android.server.telecom.AsyncRingtonePlayer;
+import com.android.server.telecom.BluetoothPhoneServiceImpl;
 import com.android.server.telecom.CallAudioManager;
 import com.android.server.telecom.CallAudioModeStateMachine;
 import com.android.server.telecom.CallAudioRouteStateMachine;
@@ -200,6 +201,7 @@ public class TelecomSystemTest extends TelecomTestCase {
     @Mock HeadsetMediaButton mHeadsetMediaButton;
     @Mock ProximitySensorManager mProximitySensorManager;
     @Mock InCallWakeLockController mInCallWakeLockController;
+    @Mock BluetoothPhoneServiceImpl mBluetoothPhoneServiceImpl;
     @Mock AsyncRingtonePlayer mAsyncRingtonePlayer;
     @Mock IncomingCallNotifier mIncomingCallNotifier;
     @Mock ClockProxy mClockProxy;
@@ -483,6 +485,7 @@ public class TelecomSystemTest extends TelecomTestCase {
                 proximitySensorManagerFactory,
                 inCallWakeLockControllerFactory,
                 () -> mAudioService,
+                (context, lock, callsManager, phoneAccountRegistrar) -> mBluetoothPhoneServiceImpl,
                 mConnServFMFactory,
                 mTimeoutsAdapter,
                 mAsyncRingtonePlayer,
