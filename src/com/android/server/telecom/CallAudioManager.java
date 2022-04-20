457f356bf350212fe9f066370b181b3d18f35b39
Grace Jia <xiaotonj@google.com>
2022-04-20 11:23:35 -0700

    Implement gathering missed information about ringing.
    Added implementation for missed reason:
        USER_MISSED_NO_ANSWER
        USER_MISSED_SHORT_RING
        USER_MISSED_DND_MODE
        USER_MISSED_LOW_RING_VOLUME
        USER_MISSED_NO_VIBRATE
        USER_MISSED_NEVER_RANG
    
    Test: atest MissedInformationTest
    Bug: 229855250, 230009891
    Change-Id: If3bd82f0bcc44ba77748b92315fa5d72aaf275eb

--- src/com/android/server/telecom/CallAudioManager.java
+++ src/com/android/server/telecom/CallAudioManager.java
@@ -468,8 +468,12 @@ public class CallAudioManager extends CallsManagerListenerBase {
     @VisibleForTesting
     public boolean startRinging() {
         synchronized (mCallsManager.getLock()) {
-            return mRinger.startRinging(mForegroundCall,
+            boolean result = mRinger.startRinging(mForegroundCall,
                     mCallAudioRouteStateMachine.isHfpDeviceAvailable());
+            if (result) {
+                mForegroundCall.setStartRingTime();
+            }
+            return result;
         }
     }
 
