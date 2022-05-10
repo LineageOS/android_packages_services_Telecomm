3d7ee3709b3a590ea48491a8b05d2929658716c7
Grace Jia <xiaotonj@google.com>
2022-05-10 16:04:33 -0700

    Use setCommunicationDevice to connect hearing aid BT devices.
    Currently we can't switch audio routes when connect to a hearing aid BT
    device and can't continue playing music via hearing aid BT device
    automatically after a call. Use setCommunicationDevice API same as LE
    audio device to solve this.
    Also fix minor bug for LE audio BT device to make sure these device
    won't be disabled unintentionally.
    
    Bug: 229358080
    Bug: 232360337
    Test: TelecomUnitTestCases, manually test the audio route can switch
    properly in a call initially routed via hearing aid BT device and can
    play music properly after the call.
    
    Change-Id: I7ff35d5eada34335fafa6f0aeaff019803541b51
    Merged-In: I7ff35d5eada34335fafa6f0aeaff019803541b51

--- src/com/android/server/telecom/bluetooth/BluetoothStateReceiver.java
+++ src/com/android/server/telecom/bluetooth/BluetoothStateReceiver.java
@@ -185,8 +185,17 @@ public class BluetoothStateReceiver extends BroadcastReceiver {
                      * is set as communication device before we can say that BT_AUDIO_IS_ON
                      */
                     if (!mBluetoothDeviceManager.setLeAudioCommunicationDevice()) {
-                        Log.w(LOG_TAG, "Device %s cannot be use as communication device.", device);
+                        Log.w(LOG_TAG,
+                                "Device %s cannot be use as LE audio communication device.",
+                                device);
                         return;
+                    }
+                } else {
+                    /* deviceType == BluetoothDeviceManager.DEVICE_TYPE_HEARING_AID */
+                    if (!mBluetoothDeviceManager.setHearingAidCommunicationDevice()) {
+                        Log.w(LOG_TAG,
+                                "Device %s cannot be use as hearing aid communication device.",
+                                device);
                     }
                 }
                 mBluetoothRouteManager.sendMessage(BT_AUDIO_IS_ON, args);
