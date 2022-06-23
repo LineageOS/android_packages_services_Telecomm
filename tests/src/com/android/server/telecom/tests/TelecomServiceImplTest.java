03bf117ddbe93b7d56e8242277c601b102c74c28
Thomas Stuart <tjstuart@google.com>
2022-06-23 14:20:30 -0700

    switch TelecomManager List getters to ParceledListSlice
    It was shown that given a large phoneAccountHandles that are
    over 1 mb, a TransactionTooLarge exception can be silently thrown
    causing an empty list to be returned.
    
    In order to prevent this behavior, all Lists that return a
    PhoneAccountHandle or PhoneAccount have been switched to
    ParceledListSlice.
    
    bug: 236263294
    
    Test: manual #1 - bug exists without fix,
          manual #2 - bug is fixed with patch,
          4 new CTS tests in
          android.telecom.cts.PhoneAccountRegistrarTest
    
    Change-Id: Ibc3814dabd59cf9f0f9505b88f2146a4c3c5e015
    Merged-In: Ibc3814dabd59cf9f0f9505b88f2146a4c3c5e015
    (cherry picked from commit 963c5c7b304442108e9edab6573dcf0176f7af08)
    Merged-In: Ibc3814dabd59cf9f0f9505b88f2146a4c3c5e015

--- tests/src/com/android/server/telecom/tests/TelecomServiceImplTest.java
+++ tests/src/com/android/server/telecom/tests/TelecomServiceImplTest.java
@@ -391,9 +391,11 @@ public class TelecomServiceImplTest extends TelecomTestCase {
         makeAccountsVisibleToAllUsers(TEL_PA_HANDLE_16, SIP_PA_HANDLE_17);
 
         assertEquals(fullPHList,
-                mTSIBinder.getCallCapablePhoneAccounts(true, DEFAULT_DIALER_PACKAGE, null));
+                mTSIBinder.getCallCapablePhoneAccounts(
+                        true, DEFAULT_DIALER_PACKAGE, null).getList());
         assertEquals(smallPHList,
-                mTSIBinder.getCallCapablePhoneAccounts(false, DEFAULT_DIALER_PACKAGE, null));
+                mTSIBinder.getCallCapablePhoneAccounts(
+                        false, DEFAULT_DIALER_PACKAGE, null).getList());
     }
 
     @SmallTest
@@ -408,7 +410,7 @@ public class TelecomServiceImplTest extends TelecomTestCase {
 
         List<PhoneAccountHandle> result = null;
         try {
-            result = mTSIBinder.getCallCapablePhoneAccounts(true, "", null);
+            result = mTSIBinder.getCallCapablePhoneAccounts(true, "", null).getList();
         } catch (SecurityException e) {
             // intended behavior
         }
@@ -436,9 +438,11 @@ public class TelecomServiceImplTest extends TelecomTestCase {
         makeAccountsVisibleToAllUsers(TEL_PA_HANDLE_16, SIP_PA_HANDLE_17);
 
         assertEquals(telPHList,
-                mTSIBinder.getPhoneAccountsSupportingScheme("tel", DEFAULT_DIALER_PACKAGE));
+                mTSIBinder.getPhoneAccountsSupportingScheme(
+                        "tel", DEFAULT_DIALER_PACKAGE).getList());
         assertEquals(sipPHList,
-                mTSIBinder.getPhoneAccountsSupportingScheme("sip", DEFAULT_DIALER_PACKAGE));
+                mTSIBinder.getPhoneAccountsSupportingScheme(
+                        "sip", DEFAULT_DIALER_PACKAGE).getList());
     }
 
     @SmallTest
@@ -454,7 +458,7 @@ public class TelecomServiceImplTest extends TelecomTestCase {
         makeAccountsVisibleToAllUsers(TEL_PA_HANDLE_16, SIP_PA_HANDLE_17);
         assertEquals(phoneAccountHandleList,
                 mTSIBinder.getPhoneAccountsForPackage(
-                        TEL_PA_HANDLE_16.getComponentName().getPackageName()));
+                        TEL_PA_HANDLE_16.getComponentName().getPackageName()).getList());
     }
 
     @SmallTest
@@ -483,7 +487,7 @@ public class TelecomServiceImplTest extends TelecomTestCase {
         when(mFakePhoneAccountRegistrar.getAllPhoneAccounts(any(UserHandle.class)))
                 .thenReturn(phoneAccountList);
 
-        assertEquals(2, mTSIBinder.getAllPhoneAccounts().size());
+        assertEquals(2, mTSIBinder.getAllPhoneAccounts().getList().size());
     }
 
     @SmallTest
