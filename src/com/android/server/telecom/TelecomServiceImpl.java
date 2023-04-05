1fce7e84b684be2b55778e0a87da10b8beeb56ed
Pranav Madapurmath <pmadapurmath@google.com>
2023-04-05 21:36:12 +0000

    Resolve account image icon profile boundary exploit.
    Because Telecom grants the INTERACT_ACROSS_USERS permission, an exploit
    is possible where the user can upload an image icon (belonging to
    another user) via registering a phone account. This CL provides a
    lightweight solution for parsing the image URI to detect profile
    exploitation.
    
    Fixes: 273502295
    Fixes: 296915211
    Test: Unit test to enforce successful/failure path
    (cherry picked from commit d0d1d38e37de54e58a7532a0020582fbd7d476b7)
    (cherry picked from commit e7d0ca3fe5be6e393f643f565792ea5e7ed05f48)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:a604311f86ea8136ca2ac9f9ff0af7fa57ee3f42)
    Merged-In: I2b6418f019a373ee9f02ba8683e5b694e7ab80a5
    Change-Id: I2b6418f019a373ee9f02ba8683e5b694e7ab80a5

--- src/com/android/server/telecom/TelecomServiceImpl.java
+++ src/com/android/server/telecom/TelecomServiceImpl.java
@@ -37,6 +37,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.pm.ParceledListSlice;
 import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
@@ -524,6 +525,9 @@ public class TelecomServiceImpl {
                                     .setGroupId(null)
                                     .build();
                         }
+
+                        // Validate the profile boundary of the given image URI.
+                        validateAccountIconUserBoundary(account.getIcon());
 
                         final long token = Binder.clearCallingIdentity();
                         try {
@@ -2110,6 +2114,23 @@ public class TelecomServiceImpl {
                 .EXTRA_DEFAULT_CALL_SCREENING_APP_COMPONENT_NAME, componentName);
             intent.setPackage(broadcastComponentName.getPackageName());
             mContext.sendBroadcast(intent);
+        }
+    }
+
+    private void validateAccountIconUserBoundary(Icon icon) {
+        // Refer to Icon#getUriString for context. The URI string is invalid for icons of
+        // incompatible types.
+        if (icon != null && (icon.getType() == Icon.TYPE_URI)) {
+            String encodedUser = icon.getUri().getEncodedUserInfo();
+            // If there is no encoded user, the URI is calling into the calling user space
+            if (encodedUser != null) {
+                int userId = Integer.parseInt(encodedUser);
+                if (userId != UserHandle.getUserId(Binder.getCallingUid())) {
+                    // If we are transcending the profile boundary, throw an error.
+                    throw new IllegalArgumentException("Attempting to register a phone account with"
+                            + " an image icon belonging to another user.");
+                }
+            }
         }
     }
 }
