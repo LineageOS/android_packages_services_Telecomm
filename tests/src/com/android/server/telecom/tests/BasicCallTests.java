29350e833aac4ec142058212a53435e39dc97271
Grant Menke <grantmenke@google.com>
2024-10-28 16:42:25 -0700

    Unbind CS if connection is not created within 15 seconds.
    This CL adds a check to ensure that connection creation occurs within 15 seconds after binding to that ConnectionService. If the connection/conference is not created in that timespan, this CL adds logic to manually unbind the ConnectionService at that point in time. This prevents malicious apps from keeping a declared permission in forever even in the background. This updated CL includes a null check to avoid a NPE that occurred in the first iteration of this change.
    
    Bug: 293458004
    Test: manually using the provided apk + atest CallsManagerTest
    Flag: EXEMPT Security High/Critical Severity CVE
    (cherry picked from commit 48d6b0df91fb1c8c0b11891f878084c6d8f9fa8a)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:87d3edd1743efae8cfb5ff60b862e6430987bc5a)
    Merged-In: If46cfa26278f09854c10266af6eaa73382f20296
    Change-Id: If46cfa26278f09854c10266af6eaa73382f20296

--- tests/src/com/android/server/telecom/tests/BasicCallTests.java
+++ tests/src/com/android/server/telecom/tests/BasicCallTests.java
@@ -993,6 +993,7 @@ public class BasicCallTests extends TelecomSystemTest {
         call.setTargetPhoneAccount(mPhoneAccountA1.getAccountHandle());
         assert(call.isVideoCallingSupportedByPhoneAccount());
         assertEquals(VideoProfile.STATE_BIDIRECTIONAL, call.getVideoState());
+        call.setIsCreateConnectionComplete(true);
     }
 
     /**
@@ -1016,6 +1017,7 @@ public class BasicCallTests extends TelecomSystemTest {
         call.setTargetPhoneAccount(mPhoneAccountA2.getAccountHandle());
         assert(!call.isVideoCallingSupportedByPhoneAccount());
         assertEquals(VideoProfile.STATE_AUDIO_ONLY, call.getVideoState());
+        call.setIsCreateConnectionComplete(true);
     }
 
     /**
