7db2aaa561704b94cc641ead5db12f7ef501f336
Pranav Madapurmath <pmadapurmath@google.com>
2025-01-02 15:04:45 -0800

    Resolve cross account user icon validation.
    Resolves a vulnerability found with the cross account user icon
    validation in StatusHint and TelecomServiceImpl (when registering a
    phone account). The reporter found that an uri formatted as `userId%`
    isn't parsed properly with the existing reference to Uri.encodedUserInfo.
    
    Bug: 376461551
    Bug: 376259166
    Flag: EXEMPT bugfix
    Test: atest TelecomServiceImplTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4c046b179d4bdffa6d35f9637b0f4cc359a67451)
    Merged-In: I7a5f64ae01eaf6a133ea04c51bd00dbe1653b74f
    Change-Id: I7a5f64ae01eaf6a133ea04c51bd00dbe1653b74f

--- src/com/android/server/telecom/TelecomServiceImpl.java
+++ src/com/android/server/telecom/TelecomServiceImpl.java
@@ -48,6 +48,7 @@ import android.provider.Settings;
 import android.telecom.Log;
 import android.telecom.PhoneAccount;
 import android.telecom.PhoneAccountHandle;
+import android.telecom.StatusHints;
 import android.telecom.TelecomAnalytics;
 import android.telecom.TelecomManager;
 import android.telecom.VideoProfile;
@@ -2121,15 +2122,13 @@ public class TelecomServiceImpl {
         // Refer to Icon#getUriString for context. The URI string is invalid for icons of
         // incompatible types.
         if (icon != null && (icon.getType() == Icon.TYPE_URI)) {
-            String encodedUser = icon.getUri().getEncodedUserInfo();
-            // If there is no encoded user, the URI is calling into the calling user space
-            if (encodedUser != null) {
-                int userId = Integer.parseInt(encodedUser);
-                if (userId != UserHandle.getUserId(Binder.getCallingUid())) {
-                    // If we are transcending the profile boundary, throw an error.
-                    throw new IllegalArgumentException("Attempting to register a phone account with"
-                            + " an image icon belonging to another user.");
-                }
+            int callingUserId = UserHandle.getCallingUserId();
+            int requestingUserId = StatusHints.getUserIdFromAuthority(
+                    icon.getUri().getAuthority(), callingUserId);
+            if(callingUserId != requestingUserId) {
+                // If we are transcending the profile boundary, throw an error.
+                throw new IllegalArgumentException("Attempting to register a phone account with"
+                        + " an image icon belonging to another user.");
             }
         }
     }
