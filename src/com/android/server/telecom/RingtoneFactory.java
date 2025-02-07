d561c9052bc83527a07fd4ea5e3181759585b1b4
Grant Menke <grantmenke@google.com>
2025-02-07 14:58:43 -0800

    Revert "Resolve cross account user ringtone validation."
    This reverts commit 5c1c52a95703b14ba2921a4d08abeb1ec592e82a.
    
    Reason for revert: root cause of custom ringtone regression (b/384520330)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d9e9fcf51a5aca840648d4acb51c3d590c8aeb08)
    Merged-In: I5484200e38b8096e594bcaf1ae37fe3d203e7004
    Change-Id: I5484200e38b8096e594bcaf1ae37fe3d203e7004

--- src/com/android/server/telecom/RingtoneFactory.java
+++ src/com/android/server/telecom/RingtoneFactory.java
@@ -74,18 +74,12 @@ public class RingtoneFactory {
         Ringtone ringtone = null;
 
         if (ringtoneUri != null && userContext != null) {
-            if (currentUserOwnsRingtone(ringtoneUri, incomingCall)) {
-                // Ringtone URI is explicitly specified and owned by the current user - try to
-                // create a Ringtone with that.
-                try {
-                    ringtone = RingtoneManager.getRingtone(
-                            userContext, ringtoneUri, volumeShaperConfig, audioAttrs);
-                } catch (Exception e) {
-                    Log.e(this, e, "getRingtone: exception while getting ringtone.");
-                }
-            } else {
-                Log.w(this, "getRingtone: Failed to verify that the custom ringtone URI"
-                        + " is owned by the current user. Falling back to the default ringtone.");
+            // Ringtone URI is explicitly specified. First, try to create a Ringtone with that.
+            try {
+                ringtone = RingtoneManager.getRingtone(
+                        userContext, ringtoneUri, volumeShaperConfig, audioAttrs);
+            } catch (Exception e) {
+                Log.e(this, e, "getRingtone: exception while getting ringtone.");
             }
         }
         if (ringtone == null) {
@@ -123,21 +117,6 @@ public class RingtoneFactory {
             }
         }
         return new Pair(ringtoneUri, ringtone);
-    }
-
-    private static boolean currentUserOwnsRingtone(Uri ringtoneUri, Call incomingCall) {
-        if (TextUtils.isEmpty(ringtoneUri.getUserInfo()) ||
-                incomingCall.getAssociatedUser() == null) {
-            return false;
-        }
-
-        UserHandle associatedUser = incomingCall.getAssociatedUser();
-        if (associatedUser == null) {
-            return false;
-        }
-
-        String currentUserId = String.valueOf(associatedUser.getIdentifier());
-        return currentUserId.equals(ringtoneUri.getUserInfo());
     }
 
     private AudioAttributes getDefaultRingtoneAudioAttributes(boolean hapticChannelsMuted) {
